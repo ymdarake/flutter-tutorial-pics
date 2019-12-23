3ac70a0380f08258b90c3f562ad267eafede0f1c
ymdarake <t.sakamoto.ouen@gmail.com>
2019-12-23 19:28:34 +1300

    [done]102: Column

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -10,8 +10,25 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Image.network(images[index].url);
+        return buildImage(images[index]);
       },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+      padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child: Image.network(image.url),
+            padding: EdgeInsets.only(bottom: 8.0),
+          ),
+          Text(image.title),
+        ],
+      ),
     );
   }
 }
